d9c263e3e1093245352c8b91da767e320c6ffa4a
maga978 <lioimagali28@gmail.com>
2026-07-29 22:40:05 -0300

    se agrego ejercicio de while

--- Condicionales/condicionales/src/App.java
+++ Condicionales/condicionales/src/App.java
@@ -184,7 +184,7 @@ switch(dia){
 
 
 
-System.out.print("ingrese un numero del 1 al 99");
+/*System.out.print("ingrese un numero del 1 al 99");
  int num = sc.nextInt();
 if(num >=10 && num<=15 ) {
  switch(num){
@@ -259,13 +259,86 @@ if(num >=10 && num<=15 ) {
                     case 9: System.out.print("nueve"); break;
                 }
             }
+}*/
+
+//sabiendo que para calcular la letra de un documento nacional de id el algoritmo es de 23 numeros del dni. dsieñar una aplicacion en la que , dado un numero de dni calcule la letra que le corresponde
+
+System.out.print("ingresa un nemero del o al 22: ");
+
+int dni = sc.nextInt(); 
+int resto = dni % 23;
+
+switch(resto){
+case 0:
+        System.out.println("La letra es: T");
+        break;
+case 1:
+        System.out.println("La letra es: R");
+        break;
+case 2:
+        System.out.println("La letra es: W");
+        break;
+case 3:
+        System.out.println("La letra es: A");
+        break;
+case 4:
+        System.out.println("La letra es: G");
+        break;
+case 5:
+        System.out.println("La letra es: M");
+        break;
+case 6:
+        System.out.println("La letra es: Y");
+        break;
+case 7:
+        System.out.println("La letra es: F");
+        break;
+case 8:
+        System.out.println("La letra es: P");
+        break;
+case 9:
+        System.out.println("La letra es: D");
+        break;
+case 10:
+        System.out.println("La letra es: X");
+        break;
+case 11:
+        System.out.println("La letra es: B");
+        break;
+case 12:
+        System.out.println("La letra es: N");
+        break;
+case 13:
+        System.out.println("La letra es: J");
+        break;
+case 14:
+        System.out.println("La letra es: Z");
+        break;
+case 15:
+        System.out.println("La letra es: S");
+        break;
+case 16:
+        System.out.println("La letra es: Q");
+        break;
+case 17:
+        System.out.println("La letra es: V");
+        break;
+case 18:
+        System.out.println("La letra es: H");
+        break;
+case 19:
+        System.out.println("La letra es: L");
+        break;
+case 20:
+        System.out.println("La letra es: C");
+        break;
+case 21:
+        System.out.println("La letra es: K");
+        break;
+case 22:
+        System.out.println("La letra es: E");
+        break;
 }
-
-
-
-
-
-
 
 
 
