d3d37e6855839069b422da620162eaaf06fad4b4
maga978 <lioimagali28@gmail.com>
2026-07-31 11:56:24 -0300

    se agrego explicacion de los bucles

---
+++ bucles/bucles/src/App.java
@@ -0,0 +1,158 @@
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+   Scanner sc = new Scanner (System.in);
+
+//BUCLE WHILE (NO EJECUTA EL CODIGO SI NO CUMPLE CON LA CONDICION PROPUESTA)
+/*WHILE(CONDICION){
+    "CODIGO"
+}
+
+ int edad;
+ System.out.print("ingrese un numero");
+ edad=sc.nextInt();
+while (edad < 18){ //este codigo se va a ejecutar solo si la edad es menor a 18 
+
+
+System.out.print("Ingrese una edad valida (18 o mas): ");
+
+edad=sc.nextInt();//SE VUELVE A PONER PARA QUE PREGUNTE DE NUVEO EL NUMERO
+}
+//si la edad es mayor a 18 no se ejecuta el while y pasa directamente a la siguiente linea
+System.out.print("usted es mayor de edad: acceso permitido");
+*/
+
+
+
+
+//BUCLE DO WHILE EL DO-WHILE SE EJECUTA AL MENOS UNA VEZ PARA VER SI LA CONDICION ES VERDADERA O NO AL REVE DE WHIE QUE PRIMERO DECIDE Y DESPUES EJECUTA
+
+
+/*do {
+    // Código que se ejecuta AL MENOS UNA VEZ
+} while (condicion); 
+
+int edad;
+
+
+do{
+System.out.print("ingrese su edad (debe ser mayor de 18 años): ");
+edad=sc.nextInt();
+}while(edad <= 18);
+System.out.print("usted es mayor de edad,acceso valido");
+
+
+while: Se usa cuando quizás no necesites ejecutar el código ni una sola vez si la condición falla de entrada.
+
+do-while: Se usa cuando necesitás obligatoriamente que el código se ejecute la primera vez (ideal para pedir datos por consola o mostrar menús de opciones).
+*/
+
+
+//BUCLE FOR CONTROLA LA CANTIDAD DE VECES QUE QUERES QUE SE REPITA UNA OPERACION
+/*  for (inicializacion; condicion; incremento){
+        int i = 0       i < 5          i++
+
+inicializacion declara una variable de contador
+condicion: Se evalúa antes de cada repetición. Mientras sea verdadera, el bucle sigue.
+Incremento (i++): Se ejecuta al final de cada vuelta para sumar o restar al contador
+ } */
+
+//TAMBIEN ESTAN LAS SALIDAS ANTICIPADAS (BREAK/ CONTINUE)
+//BREAK UNA VEZ QUE SE ENCONTRO LA CONDICION PROPUESTA SE CORTA EL BUCLE
+
+/*for (int i = 1; i <= 100; i++) {
+    if (i == 7) {
+        System.out.println("¡Encontré el 7! Cortando el bucle...");
+        break; // <--- SALE ANTICIPADAMENTE ACÁ
+    }
+    System.out.println("Buscando, voy por el número: " + i);
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ }
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
